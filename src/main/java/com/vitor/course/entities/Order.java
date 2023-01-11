71bd4d5c532cbb0c43ea0a022f47e8e682db254a
limavit <limavit@gmail.com>
2023-01-11 10:33:59 -0300

    subtotal e total methods

--- src/main/java/com/vitor/course/entities/Order.java
+++ src/main/java/com/vitor/course/entities/Order.java
@@ -88,6 +88,13 @@ public class Order implements Serializable{
 	public Set<OrderItem> getItems(){
 		return items;
 	}
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem orderItem : items) {
+			sum += orderItem.getSubTotal();
+		}
+		return sum;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
