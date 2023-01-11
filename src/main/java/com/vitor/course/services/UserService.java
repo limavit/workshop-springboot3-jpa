a528612e779ab82bb54497cd13a958e2b3fa2ac8
limavit <limavit@gmail.com>
2023-01-11 17:45:55 -0300

    Exception handling - delete

--- src/main/java/com/vitor/course/services/UserService.java
+++ src/main/java/com/vitor/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.vitor.course.entities.User;
 import com.vitor.course.repositories.UserRepository;
+import com.vitor.course.services.exceptions.DatabaseException;
 import com.vitor.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -28,7 +31,14 @@ public class UserService {
 		
 	}
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	public User update(Long id, User obj) {
 		User entity = repository.getReferenceById(id);
