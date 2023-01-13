53645fbe3a6bc245a7e66796c585da1e83f77db3
limavit <limavit@gmail.com>
2023-01-13 15:51:06 -0300

    exception handling update

--- src/main/java/com/vitor/course/services/UserService.java
+++ src/main/java/com/vitor/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.vitor.course.repositories.UserRepository;
 import com.vitor.course.services.exceptions.DatabaseException;
 import com.vitor.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -41,9 +43,14 @@ public class UserService {
 		
 	}
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
